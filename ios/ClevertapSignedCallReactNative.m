459edb4a4bc7d5a35470016f7f9e6c9aec40ce9e
SonalKachare <sonal@clevertap.com>
2023-02-24 17:44:37 +0530

    Added iOS extern functions

--- ios/ClevertapSignedCallReactNative.m
+++ ios/ClevertapSignedCallReactNative.m
@@ -1,10 +1,24 @@
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import <SignedCallSDK/SignedCallSDK.h>
 
 @interface RCT_EXTERN_MODULE(ClevertapSignedCallReactNative, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(
+                  initSDK:(NSDictionary *)initOptions
                  withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+                 withRejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  call:(NSString *)receiverCuid
+                  withContext: (NSString *)callContext
+                  withCallProperties: (NSDictionary *)callProperties
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(setDebugLevel:(int)logLevel)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/ClevertapSignedCallReactNative.swift
+++ ios/ClevertapSignedCallReactNative.swift
@@ -1,8 +1,110 @@
-@objc(ClevertapSignedCallReactNative)
-class ClevertapSignedCallReactNative: NSObject {
+import SignedCallSDK
+import CleverTapSDK
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+@objc(ClevertapSignedCallReactNative)
+class ClevertapSignedCallReactNative: RCTEventEmitter {
+    
+    var hasListeners = false
+    
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.callStatus(notification:)), name: NSNotification.Name(rawValue: SCConstant.messageReceived), object: nil)
+    }
+    
+    @objc(setDebugLevel:)
+    func setDebugLevel(logLevel: Int) -> Void {
+        guard logLevel >= 0 else {
+            SignedCall.isLoggingEnabled = false
+            return
+        }
+        SignedCall.isLoggingEnabled = true
+    }
+    
+    @objc(call:withContext:withCallProperties:withResolver:withRejecter:)
+    func call(receiverCuid: String?, callContext: String?, callProperties: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        guard let callContext = callContext, let receiverCuid = receiverCuid else {
+            return
+        }
+        var customMetaData: SCCustomMetadata?
+        if let callDetails = callProperties as? [String: Any?],
+           let initiatorImage = callDetails[SCConstant.initiatorImage] as? String,
+           let receiverImage = callDetails[SCConstant.receiverImage] as? String {
+            
+            customMetaData = SCCustomMetadata(initiatorImage: initiatorImage, receiverImage: receiverImage)
+        }
+        
+        let callOptions = SCCallOptionsModel(context: callContext, receiverCuid: "sonalw", customMetaData: customMetaData)
+        SignedCall.call(callOptions: callOptions) { result in
+            switch result {
+            case .success(let value):
+                resolve(value)
+            case .failure(let error):
+                reject("\(error.errorCode)",
+                       error.errorDescription,
+                       error)
+            }
+        }
+    }
+    
+    @objc(initSDK:withResolver:withRejecter:)
+    func initSDK(initOptions: NSDictionary, resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock) -> Void {
+        DispatchQueue.main.async {
+            SignedCall.registerVoIP(withRootView: UIViewController())
+        }
+        
+        guard let initOptionsDict = initOptions as? [String: Any?] else {
+            return
+        }
+        
+        SignedCall.cleverTapInstance = CleverTap.sharedInstance()
+        SignedCall.initSDK(withInitOptions: initOptionsDict) { result in
+            switch result {
+            case .success(let value):
+                resolve(["isSuccessful": value])
+            case .failure(let error):
+                reject("\(error.errorCode)",
+                       error.errorDescription,
+                       error)
+            }
+        }
+    }
+    
+    
+// MARK: - Call Event Handling
+    
+    override func supportedEvents() -> [String]! {
+        [SCConstant.onCallStatusChanged]
+    }
+    
+    override func startObserving() {
+        hasListeners = true
+    }
+    
+    override func stopObserving() {
+        hasListeners = false
+    }
+    
+    @objc func callStatus(notification: Notification) {
+        let message = notification.userInfo?[SCConstant.callStatus]
+        
+        guard let callValue = message as? Int,
+              let callEvent = SCCallStatus(rawValue: callValue) else {
+            //  os_log("Unknown call event raised: %{public}@", log: logValue, type: .default, notification.object.debugDescription)
+            return
+        }
+        
+        switch callEvent {
+            
+        case .CALL_CANCEL, .CALL_DECLINED, .CALL_MISSED, .CALL_ANSWERED, .CALL_CONNECTED, .RECEIVER_BUSY_ON_ANOTHER_CALL, .CALL_OVER :
+            handleCallEvent(SCCallEvent(rawValue: callValue).value)
+        default: break
+        }
+    }
+        
+    func handleCallEvent(_ callEvent: String) {
+        print(callEvent)
+        if hasListeners {
+            sendEvent(withName: SCConstant.onCallStatusChanged, body: callEvent)
+        }
+   }
 }
