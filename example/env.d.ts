60d2621db64d10738cdbbbc8d7d6ed8726659230
Shivam Sharma <shivam.sharma@clevertap.com>
2024-12-30 02:37:09 +0530

    Remove .env from version control

--- example/env.d.ts
+++ example/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
-  export const SC_ACCOUNT_ID: string;
-  export const SC_API_KEY: string;
+  export const SC_ACCOUNT_ID: string = '61a52046f56a14cb19a1e9cc';
+  export const SC_API_KEY: string =
+    '9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd';
 }
