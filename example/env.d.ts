2693fd02b8363b671eaa152a3fdcc1c26f7a3327
Shivam Sharma <shivam.sharma@clevertap.com>
2025-01-13 17:11:16 +0530

    update gitignore

--- example/env.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@env' {
-  export const SC_ACCOUNT_ID: string = '61a52046f56a14cb19a1e9cc';
-  export const SC_API_KEY: string =
-    '9dcced09dae16c5e3606c22346d92361b77efdb360425913850bea4f22d812dd';
-}
